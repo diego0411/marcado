9037c3266c13b1617112c41dbad86826aa0256f1
diego0411 <53972010+diego0411@users.noreply.github.com>
2025-02-05 09:48:51 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  timezone: "America/La_Paz",
 });
 
 module.exports = db;
